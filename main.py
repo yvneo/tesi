683e294c162c5e9b248fd8dbc9ddf31f04d4b54e
yvneo <anna.martini2004@gmail.com>
2026-03-18 14:45:59 +0100

    aggiunta degli scenari, scaler per ciascuna feature di traffico,  modifica ai grafici

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from data_loader import load_and_split_data
 from feature_engineering import extract_features
 from model_utils import train_and_evaluate_model
+import numpy as np
+
 
 DATASET_PATH = "/storage_14tb/workspace_idio/UniVR_Data/dataset_df_exact_no0load_saturated_handshake_tail_no0loadFilter_extended_94bde95_7e0dbc17/"
 
@@ -15,8 +17,31 @@ print(f"Matrice Home. Forma: {X_home.shape}")
 print(f"Matrice UniVR. Forma: {X_univr.shape}") 
 
 print("\n--- FASE 3: Addestramento e Valutazione Modello ---")
-results_df, report_df = train_and_evaluate_model(X_home, y_home, X_univr, y_univr)
 
-report_df.to_csv("risultati/RFreport.csv", index=False)
-results_df.to_csv("risultati/RFresults.csv", index=False)
+common_classes = np.intersect1d(np.unique(y_home), np.unique(y_univr))
 
+filter_home = np.isin(y_home, common_classes)
+filter_univr = np.isin(y_univr, common_classes)
+
+X_home_filtered = X_home[filter_home]
+y_home_filtered = y_home[filter_home]
+X_univr_filtered = X_univr[filter_univr]
+y_univr_filtered = y_univr[filter_univr]
+print(f"Campioni Home dopo filtro: {len(y_home_filtered)} | Campioni UniVR dopo filtro: {len(y_univr_filtered)}")
+
+#SCENARIO 1: Addestramento su Home, test su UniVR
+print("\nSCENARIO 1: Addestramento su Home, test su UniVR")
+report_scenario_1 = train_and_evaluate_model(X_home_filtered, y_home_filtered, X_univr_filtered, y_univr_filtered, scenario_name="Home_vs_UniVR")
+report_scenario_1.to_csv("report_scenario_1.csv", index=False)
+
+#SCENARIO 2: Addestramento su UniVR, test su Home
+print("\nSCENARIO 2: Addestramento su UniVR, test su Home")
+report_scenario_2 = train_and_evaluate_model(X_univr_filtered, y_univr_filtered, X_home_filtered, y_home_filtered, scenario_name="UniVR_vs_Home")
+report_scenario_2.to_csv("report_scenario_2.csv", index=False)
+
+#SCENARIO 3: Addestramento su dati combinati, test su Home
+print("\nSCENARIO 3: Addestramento su dati combinati, test su Home")
+X_combined = np.vstack((X_home_filtered, X_univr_filtered))
+y_combined = np.concatenate((y_home_filtered, y_univr_filtered))
+report_scenario_3 = train_and_evaluate_model(X_combined, y_combined, X_home_filtered, y_home_filtered, scenario_name="Combined_vs_Home")
+report_scenario_3.to_csv("report_scenario_3.csv", index=False)
--- model_utils.py
+++ model_utils.py
@@ -14,125 +14,137 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
-def train_and_evaluate_model(X_home, y_home, X_univr, y_univr):
+def train_and_evaluate_model(X_train_data, y_train_data, X_test_ext, y_test_ext, scenario_name):
 
     #trovo le classi comuni tra i due dataset
-    common_classes = np.intersect1d(np.unique(y_home), np.unique(y_univr))
+    #common_classes = np.intersect1d(np.unique(y_train_data), np.unique(y_test_ext))
 
     #filtro Home
-    mask_home = np.isin(y_home, common_classes)
-    X_home = X_home[mask_home]
-    y_home = y_home[mask_home]
+    #mask_home = np.isin(y_train_data, common_classes)
+    #X_home = X_train_data[mask_home]
+    #y_home = y_train_data[mask_home]
 
     #filtro UniVR
-    mask_univr = np.isin(y_univr, common_classes)
-    X_univr = X_univr[mask_univr]
-    y_univr = y_univr[mask_univr]
-    print("Numero campioni Home dopo filtro:", len(y_home))
-    print("Numero campioni UniVR dopo filtro:", len(y_univr))
+    #mask_univr = np.isin(y_test_ext, common_classes)
+    #X_univr = X_test_ext[mask_univr]
+    #y_univr = y_test_ext[mask_univr]
+
+    #print("Numero campioni Home dopo filtro:", len(y_home))
+    #print("Numero campioni UniVR dopo filtro:", len(y_univr))
     
-    #codifica le etichette
+    # 1. CODIFICA DELLE ETICHETTE 
     label_encoder = LabelEncoder()
-    y_home_encoded = label_encoder.fit_transform(y_home)
-    y_univr_encoded = label_encoder.transform(y_univr) 
-    class_names = label_encoder.classes_ #nomi delle classi per grafici
+    y_train_encoded = label_encoder.fit_transform(y_train_data) #imparo le classi dal training set
+    y_ext_encoded = label_encoder.transform(y_test_ext) #applico la stessa codifica al test set esterno
+    class_names = label_encoder.classes_
 
-    #configuro il K-Fold Cross Validation con K=5
+    # 2. CONFIGURAZIONE DELLA CROSS-VALIDATION
     kf = KFold(n_splits=5, shuffle=True, random_state=42)
-    results = []
     results_report = []
+    cm_internal_list = [] #per test fatti sulla stessa location di train
+    cm_external_list = [] #per test fatti su location diversa da quella di train
 
-    cm_home_list = [] #lista per matrici di confusione dei fold 
-    cm_univr_list  = []
+    #definisco i gruppi di feature per lo scaling: L4_payload_lenght, iat_micros, packet_dir, TCP_win_size
+    feature_groups =[range(0, 10), range(10, 20), range(20, 30), range(30, 40)]
 
-    for fold, (train_index, test_index) in enumerate(kf.split(X_home)): #in ogni fold prendo 4 parti per il training e 1 parte per il test
-        #divido i dati in training e test set
-        X_train, X_test = X_home[train_index], X_home[test_index]
-        y_train, y_test = y_home_encoded[train_index], y_home_encoded[test_index]
 
-        #eseguo normalizzazione con MinMaxScaler
-        scaler = MinMaxScaler()
-        X_train_scaled = scaler.fit_transform(X_train) #solo su training set
-        X_test_scaled = scaler.transform(X_test) 
-        X_univr_scaled = scaler.transform(X_univr) #applico la stessa normalizzazione anche a UniVR
+    for fold, (train_index, test_index) in enumerate(kf.split(X_train_data)):
+        
+        X_train, X_val = X_train_data[train_index], X_train_data[test_index]
+        y_train, y_val = y_train_encoded[train_index], y_train_encoded[test_index]
 
-        #addestro il modello 
-        model = RandomForestClassifier(n_estimators=100, random_state=42, class_weight='balanced') #class_weight bilancia le classi in caso di sbilanciamento
+        # 3.SCALING 
+        X_train_scaled = np.zeros_like(X_train, dtype=float)
+        X_val_scaled = np.zeros_like(X_val, dtype=float)
+        X_ext_scaled = np.zeros_like(X_test_ext, dtype=float)
+
+        for group in feature_groups:
+            scaler = MinMaxScaler()
+            data_to_fit = X_train[:, group].reshape(-1, 1)
+            scaler.fit(data_to_fit)
+            for i in group:
+                X_train_scaled[:, i] = scaler.transform(X_train[:, i].reshape(-1, 1)).flatten()
+                X_val_scaled[:, i] = scaler.transform(X_val[:, i].reshape(-1, 1)).flatten()
+                X_ext_scaled[:, i] = scaler.transform(X_test_ext[:, i].reshape(-1, 1)).flatten()
+
+        # 4. ADDDESTRAMENTO MODELLO
+        model = RandomForestClassifier(n_estimators=100, random_state=42, class_weight='balanced')
         model.fit(X_train_scaled, y_train)
 
-        #valuto il modello sia su Home che su UniVR
-        y_pred_home = model.predict(X_test_scaled) #modello prova a indovinare le etichette del test set di Home
-        y_pred_univr = model.predict(X_univr_scaled)
+        # 5. PREDIZIONI
+        y_pred_int = model.predict(X_val_scaled)
+        y_pred_ext = model.predict(X_ext_scaled)
 
-        #calcolo delle metriche f1 e balanced accuracy
-        f1_home = f1_score(y_test, y_pred_home, average='macro')
-        f1_univr = f1_score(y_univr_encoded, y_pred_univr, average='macro')
-        bal_acc_home = balanced_accuracy_score(y_test, y_pred_home)
-        bal_acc_univr = balanced_accuracy_score(y_univr_encoded, y_pred_univr)
+        # 6. CALCOLO METRICHE
+        f1_int = f1_score(y_val, y_pred_int, average='macro')
+        f1_ext = f1_score(y_ext_encoded, y_pred_ext, average='macro')
+        bal_acc_int = balanced_accuracy_score(y_val, y_pred_int)
+        bal_acc_ext = balanced_accuracy_score(y_ext_encoded, y_pred_ext)
 
-        #calcolo delle matrici di confusione 
-        cm_home = confusion_matrix(y_test, y_pred_home, labels = range(len(class_names)), normalize='true') 
-        cm_univr = confusion_matrix(y_univr_encoded, y_pred_univr, labels = range(len(class_names)), normalize='true')
+        results_report.append({
+            'fold': fold+1, 
+            'f1_Internal': f1_int, 'bal_acc_Internal': bal_acc_int,
+            'f1_External': f1_ext, 'bal_acc_External': bal_acc_ext 
+        })
 
-        cm_home_list.append(cm_home)
-        cm_univr_list.append(cm_univr)
+        # 7. MATRICI DI CONFUSIONE NORMALIZZATE IN PERCENTUALE
+        cm_int = confusion_matrix(y_val, y_pred_int, labels=range(len(class_names)), normalize='true') * 100
+        cm_ext = confusion_matrix(y_ext_encoded, y_pred_ext, labels=range(len(class_names)), normalize='true') * 100
+
+        cm_internal_list.append(cm_int)
+        cm_external_list.append(cm_ext)
         
-        for real, pred in zip(y_test, y_pred_home):
-            #zip mi mette risposta vera e quella predetta una accanto all'altra
-            results.append({'fold': fold+1, 'Domain': 'Home', 'Ground Truth': label_encoder.inverse_transform([real])[0], 'Predicted': label_encoder.inverse_transform([pred])[0]}) 
-
-        for real, pred in zip(y_univr_encoded, y_pred_univr):
-            results.append({'fold': fold+1, 'Domain': 'UniVR', 'Ground Truth': label_encoder.inverse_transform([real])[0], 'Predicted': label_encoder.inverse_transform([pred])[0]}) 
-
-        results_report.append({'fold': fold+1, 'f1_home': f1_home, 'bal_acc_home': bal_acc_home, 'f1_univr': f1_univr, 'bal_acc_univr': bal_acc_univr })  #salvo statistiche generali 
-
-    df_results = pd.DataFrame(results)
+    # 8. REPoRT E VISUALIZZAZIONE
     df_report = pd.DataFrame(results_report)
+    cm_internal_avg = np.mean(cm_internal_list, axis=0)
+    cm_external_avg = np.mean(cm_external_list, axis=0)
 
-    #calvolo la matrice di confusione media
-    cm_home_avg = np.mean(cm_home_list, axis=0)
-    cm_univr_avg = np.mean(cm_univr_list, axis=0)
+    # Visualizzazione
+    plot_results(df_report, cm_internal_avg, cm_external_avg, class_names, scenario_name)
 
-    #visualizzazione
-    plot_results(df_report, cm_home_avg, cm_univr_avg, class_names)
-
-    return df_results, df_report
+    return df_report
 
 
-def plot_results(df_report, cm_home_avg, cm_univr_avg, class_names):
+
+def plot_results(df_report, cm_internal_avg, cm_external_avg, class_names, scenario_name):
     #preparazione dei dati per il barplot
     metrics = ['f1', 'bal_acc']
+    domains = {'Internal', 'External'}
     plot_data = []
-    for metric in metrics: #per ogni metrica (f1 e balanced accuracy) prendo i valori medi e deviazione standard per Home e UniVR
-        for domain in ['home', 'univr']:
-            col_name = f"{metric}_{domain}"
-            mean_value = df_report[col_name].mean()
-            std_value = df_report[col_name].std()
-            plot_data.append({'Metric': metric, 'Domain': domain, 'Mean': mean_value, 'Std': std_value})
+
+    for _, row in df_report.iterrows():
+        for m in metrics:
+            for d in domains:
+                plot_data.append({
+                    'Metric': m,
+                    'Location': d,
+                    'Score': row[f"{m}_{d}"]
+                })
 
     plot_df = pd.DataFrame(plot_data)
 
     plt.figure(figsize=(10, 6))
     #asse x metrica, y valore medio
-    sns.barplot(data = plot_df, x='Metric', y='Mean', hue='Domain', capsize=0.1)
-    plt.title('Performance Comparison: Home vs UniVR (Mean ± Std)')
-    plt.ylabel('Mean Score')
+    sns.barplot(data = plot_df, x='Metric', y='Score', hue='Location', capsize=0.1, errorbar='sd')
+    plt.title(f'Performance Scenario: {scenario_name}')
     plt.ylim(0, 1.1)
-    plt.savefig(f"grafici/barplot_performance.png", dpi=300, bbox_inches='tight')
+    plt.savefig(f"grafici/barplot_performance_{scenario_name}.png", dpi=300, bbox_inches='tight')
     plt.show()
 
     #heatmap 
-    fig, axes = plt.subplots(1, 2, figsize=(16, 6))
-    sns.heatmap(cm_home_avg, annot=True, fmt=".2f", cmap='Blues', xticklabels=class_names, yticklabels=class_names, ax=axes[0])
-    axes[0].set_title('Average Confusion Matrix - Home')
-    axes[0].set_xlabel('Predicted')
-    axes[0].set_ylabel('True')
+    fig, axes = plt.subplots(1, 2, figsize=(18, 7))
+    sns.heatmap(cm_internal_avg, annot=True, fmt=".1f", cmap='Blues', 
+                xticklabels=class_names, yticklabels=class_names, ax=axes[0])
+    axes[0].set_title('Avg Confusion Matrix % - Internal ({scenario_name})')
+    axes[0].set_xlabel('Predicted Label')
+    axes[0].set_ylabel('True Label')
 
-    sns.heatmap(cm_univr_avg, annot=True, fmt=".2f", cmap='Blues', xticklabels=class_names, yticklabels=class_names, ax=axes[1])
-    axes[1].set_title('Average Confusion Matrix - UniVR')
-    axes[1].set_xlabel('Predicted')
-    axes[1].set_ylabel('True')
+    sns.heatmap(cm_external_avg, annot=True, fmt=".1f", cmap='Blues', 
+                xticklabels=class_names, yticklabels=class_names, ax=axes[1])
+    axes[1].set_title('Avg Confusion Matrix % - External ({scenario_name})')
+    axes[1].set_xlabel('Predicted Label')
+    axes[1].set_ylabel('True Label')
 
     plt.tight_layout()
-    plt.savefig(f"grafici/heatmap_confusion_matrix.png", dpi=300, bbox_inches='tight')
+    plt.savefig(f"grafici/heatmap_{scenario_name}.png", dpi=300)
     plt.show()
